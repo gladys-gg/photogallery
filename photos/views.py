0f153949e6d633564b814571c566a4aa2fc68b39
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-05-29 01:51:58 +0300

    created the views

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
+import datetime as dt
 
 # Create your views here.
+def gallery(request):
+    return render(request,'photos/gallery.html')
+
+def photo(request,photo_id):
+    return render(request,'photos/photo.html')
