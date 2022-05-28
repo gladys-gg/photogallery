7329051f7bbd15425bfce30651a3c04bcde1df90
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-05-29 01:39:51 +0300

    Added the location class in the apps model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+
+class Location(models.Model):
+    photo_location = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.photo_location
+    
+    def save_location(self):
+        self.save()
+    
+    def delete_location(self):
+        self.delete()
