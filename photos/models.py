8399a8db7f5f5904d3a0a1fa62ff5378d4c7dca0
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-05-29 01:42:00 +0300

    Added the category class in the apps model

--- photos/models.py
+++ photos/models.py
@@ -14,3 +14,19 @@ class Location(models.Model):
     
     def delete_location(self):
         self.delete()
+        
+class Category(models.Model):
+    photo_category = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.photo_category
+    
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+    
+    def update_category(self, update):
+        self.photo_category = update
+        self.save()
