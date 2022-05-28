c370a261a1d923df8249c6ccf99cafda75e07137
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-05-29 01:37:58 +0300

    Created the database and did the migrations

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'photos',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'photoshare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'HOST':'localhost',
+        'NAME':'photogallery',
+        'USER':'acer',
+        'PASSWORD':'password',
+        'PORT':'',
     }
 }
 
